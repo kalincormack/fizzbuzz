2369e5b6431f35823a15e8546da796ad9b6449f1
Kalin Cormack <kalincormack@Kalins-MacBook-Air.local>
2021-09-22 13:27:04 -0400

    setup testing and smoke test

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,11 @@
-
+'''
+An example of a fizzbuzz program solution written using TDD
+'''
 import random
 n = random.randint(1,100)
 print (n)
+def fizz(x):
+    '''
+returns 'fizz' if x is a multiple of 3, otherwise x
+    '''
+    return fizz
---
+++ fizzbuzz_test.py
@@ -0,0 +1,12 @@
+'''
+Unit tests for the TDD fizzbuzz program solution.
+'''
+
+from fizzbuzz import fizz
+
+def describe_a_fizzbuzz_program_that():
+    def returns_fizz_if_the_input_is_a_multiple_of_3():
+        assert fizz(3) = 'fizz'
+        assert fizz(5) = 5
+        
+
